117d49c95b0bab064748b5b84c4ff1f4781d84d1
ClickQuack <ClickQuacky@gmail.com>
2024-08-17 23:34:09 -0500

    test build

--- Macro/.venv/Scripts/activate.bat
+++ Macro/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=d:\PythonStuff\Macro\Macro\.venv
+set VIRTUAL_ENV=D:\ProgrammingStuff\Macro\Macro\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
